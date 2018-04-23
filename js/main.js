f6933760ec918b059c72496522a08c16086d4bb7
Christian Wieprecht <christian.wieprecht@udo.edu>
2018-04-23 20:27:48 +0200

    Removed unused jsfxr.

--- js/main.js
+++ js/main.js
@@ -213,10 +213,6 @@ function updateIngredients(dt) {
           if (pots[p].check(ingredients[i])) {
             removeIngredientImage(ingredients[i]);
             remove = true;
-            //var soundURL = jsfxr([0,,0.1812,,0.1349,0.4524,,0.2365,,,,,,0.0819,,,,,1,,,,,0.5]);
-            //var player = new Audio();
-            //player.src = soundURL;
-            //player.play();
           }
         }
       }
