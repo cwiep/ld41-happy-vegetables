b5b85058524368681488523f618af335605c1df3
Christian Wieprecht <christian.wieprecht@udo.edu>
2018-05-06 15:08:39 +0200

    Added better level progression for first levels.

--- js/levels.js
+++ js/levels.js
@@ -3,49 +3,55 @@ $.Levels = [
     pots: [false, true, false],
     bottomLane: false,
     speed: 0.15,
-    targetPots: 3,
-    monster: false
+    targetPots: 1,
+    monster: false,
+    bg: "res/bg1.png",
+    ingredients: ["potato"],
+    hint: "Click potatos to cut them",
+    loseable: false
   },
   {
-    pots: [false, true, true],
+    pots: [false, true, false],
     bottomLane: false,
     speed: 0.15,
-    targetPots: 5,
-    monster: false
+    targetPots: 1,
+    monster: false,
+    bg: "res/bg1.png",
+    ingredients: ["potato", "carot"],
+    hint: "Order of vegetables does not matter",
+    loseable: false
   },
   {
-    pots: [true, true, false],
-    bottomLane: true,
+    pots: [false, true, false],
+    bottomLane: false,
+    speed: 0.15,
+    targetPots: 1,
+    monster: false,
+    bg: "res/bg1.png",
+    ingredients: ["potato", "carot", "onion"],
+    hint: "Don't waste vegetables",
+    loseable: true
+  },
+  {
+    pots: [false, true, false],
+    bottomLane: false,
     speed: 0.2,
-    targetPots: 10,
-    monster: false
+    targetPots: 3,
+    monster: false,
+    bg: "res/bg1.png",
+    ingredients: ["potato", "carot", "onion", "brocoli"],
+    hint: "Reach the specified number of pots to finish a level",
+    loseable: true
   },
   {
     pots: [true, false, true],
     bottomLane: true,
     speed: 0.2,
-    targetPots: 10,
-    monster: false
-  },
-  {
-    pots: [true, true, true],
-    bottomLane: true,
-    speed: 0.25,
-    targetPots: 15,
-    monster: false
-  },
-  {
-    pots: [true, true, true],
-    bottomLane: true,
-    speed: 0.25,
-    targetPots: 15,
-    monster: false
-  },
-  {
-    pots: [true, true, true],
-    bottomLane: true,
-    speed: 0.3,
-    targetPots: 15,
-    monster: true
+    targetPots: 5,
+    monster: false,
+    bg: "res/bg2.png",
+    ingredients: ["potato", "carot", "onion", "brocoli"],
+    hint: "Get a second chance for each vegetable",
+    loseable: true
   }
 ];
