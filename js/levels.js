16a15be961f0b0d479ee898008df0331e7925df4
Christian Wieprecht <christian.wieprecht@udo.edu>
2018-05-06 18:45:03 +0200

    Planned level progression.

--- js/levels.js
+++ js/levels.js
@@ -25,12 +25,12 @@ $.Levels = [
     pots: [false, true, false],
     bottomLane: false,
     speed: 0.15,
-    targetPots: 1,
+    targetPots: 3,
     monster: false,
     bg: "res/bg1.png",
-    ingredients: ["potato", "carot", "onion"],
-    hint: "Don't cut the wrong\nvegetables",
-    loseable: true
+    ingredients: ["potato", "carot"],
+    hint: "Reach the specified number\nof pots to finish a level",
+    loseable: false
   },
   {
     pots: [false, true, false],
@@ -39,10 +39,23 @@ $.Levels = [
     targetPots: 3,
     monster: false,
     bg: "res/bg1.png",
-    ingredients: ["potato", "carot", "onion", "brocoli"],
-    hint: "Reach the specified number\nof pots to finish a level",
+    ingredients: ["potato", "carot", "onion"],
+    hint: "Don't cut the wrong\nvegetables",
     loseable: true
   },
+  // 5
+  {
+    pots: [true, false, true],
+    bottomLane: true,
+    speed: 0.2,
+    targetPots: 5,
+    monster: false,
+    bg: "res/bg2.png",
+    ingredients: ["potato", "carot", "onion"],
+    hint: "Get a second chance for\neach vegetable",
+    loseable: true
+  },
+  // 6
   {
     pots: [true, false, true],
     bottomLane: true,
@@ -51,7 +64,47 @@ $.Levels = [
     monster: false,
     bg: "res/bg2.png",
     ingredients: ["potato", "carot", "onion", "brocoli"],
-    hint: "Get a second chance for\neach vegetable",
+    hint: "Get some broccoli",
+    loseable: true
+  },
+  // 7
+  {
+    pots: [true, false, true],
+    bottomLane: true,
+    speed: 0.25,
+    targetPots: 10,
+    monster: false,
+    bg: "res/bg2.png",
+    ingredients: ["potato", "carot", "onion", "brocoli"],
+    hint: "More soups,\nfaster vegetables",
+    loseable: true
+  },
+  // 8
+  {
+    pots: [true, true, true],
+    bottomLane: true,
+    speed: 0.25,
+    targetPots: 10,
+    monster: false,
+    bg: "res/bg2.png",
+    ingredients: ["potato", "carot", "onion", "brocoli"],
+    hint: "All the pots",
+    loseable: true
+  },
+  // 9
+  {
+    pots: [true, true, true],
+    bottomLane: true,
+    speed: 0.3,
+    targetPots: 10,
+    monster: false,
+    bg: "res/bg2.png",
+    ingredients: ["potato", "carot", "onion", "brocoli"],
+    hint: "Even faster",
     loseable: true
   }
+  // 10 - monster easy one pot
+  // 11 - monster two pots on the right
+  // 12 - monster all pots
+  // 13 - monster 0.4 speed all pots
 ];
