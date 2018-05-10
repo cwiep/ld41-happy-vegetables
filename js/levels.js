23e00795280590187639c5cdaa659c1f7abeccd0
Christian Wieprecht <christian.wieprecht@udo.edu>
2018-05-10 19:55:27 +0200

    Added final levels.

--- js/levels.js
+++ js/levels.js
@@ -102,9 +102,65 @@ $.Levels = [
     ingredients: ["potato", "carot", "onion", "brocoli"],
     hint: "Even faster",
     loseable: true
-  }
+  },
   // 10 - monster easy one pot
+  {
+    pots: [true, true, false],
+    bottomLane: true,
+    speed: 0.3,
+    targetPots: 10,
+    monster: true,
+    monsterSpeed: 0,
+    monsterTimeToEat: 5000,
+    monsterStunTime: 3000,
+    bg: "res/bg2.png",
+    ingredients: ["potato", "carot", "onion", "brocoli"],
+    hint: "Beware the troll,\nit is hungry, too",
+    loseable: true
+  },
   // 11 - monster two pots on the right
+  {
+    pots: [false, true, true],
+    bottomLane: true,
+    speed: 0.3,
+    targetPots: 10,
+    monster: true,
+    monsterSpeed: 0,
+    monsterTimeToEat: 4000,
+    monsterStunTime: 1000,
+    bg: "res/bg2.png",
+    ingredients: ["potato", "carot", "onion", "brocoli"],
+    hint: "The troll wants more",
+    loseable: true
+  },
   // 12 - monster all pots
-  // 13 - monster 0.4 speed all pots
+  {
+    pots: [true, true, true],
+    bottomLane: true,
+    speed: 0.3,
+    targetPots: 10,
+    monster: true,
+    monsterSpeed: -0.1,
+    monsterTimeToEat: 4000,
+    monsterStunTime: 1000,
+    bg: "res/bg2.png",
+    ingredients: ["potato", "carot", "onion", "brocoli"],
+    hint: "IT MOVES!",
+    loseable: true
+  },
+  // 13 - monster speed all pots
+  {
+    pots: [true, true, true],
+    bottomLane: true,
+    speed: 0.3,
+    targetPots: 10,
+    monster: true,
+    monsterSpeed: -0.2,
+    monsterTimeToEat: 3000,
+    monsterStunTime: 1000,
+    bg: "res/bg2.png",
+    ingredients: ["potato", "carot", "onion", "brocoli"],
+    hint: "Finale",
+    loseable: true
+  }  
 ];
